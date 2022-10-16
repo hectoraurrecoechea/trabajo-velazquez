a9d843d1eb5de0a513fa459f37dad3bc1b00db71
Hector Aurrecoechea <labaladelcerrato@gmail.com>
2022-10-17 00:43:29 +0200

    hacer la foto grande

--- javaScript.js
+++ javaScript.js
@@ -13,3 +13,17 @@ tabs.forEach((tab, index) => {
         tabs[index].classList.add('is-active');
     });
 });
+
+
+
+const fulImgBox = document.getElementById("fulImgBox"),
+    fulImg = document.getElementById("fulImg")
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex"
+    fulImg.src = reference
+}
+
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
